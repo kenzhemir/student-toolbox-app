e8218f3a7894aadf23b3ed8606ea2b832d837451
Miras <miras.kenzhegaliyev@nu.edu.kz>
2017-07-31 14:28:44 +0600

    Added sections thumbnails in Main activity

--- app/src/main/java/com/mirka/app/studenttoolboxrevised/MainActivity.java
+++ app/src/main/java/com/mirka/app/studenttoolboxrevised/MainActivity.java
@@ -2,6 +2,8 @@ package com.mirka.app.studenttoolboxrevised;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sectionOnClickListener(View view){
+        Toast.makeText(this, view.getId() + " clicked", Toast.LENGTH_SHORT).show();
     }
 }
